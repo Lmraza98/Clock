33f86bb5fa1ee9bfb9db71ede694f1722287f896
Lucas Raza <razal@wit.edu>
2018-12-29 11:17:53 -0500

    -

--- clock.js
+++ clock.js
@@ -2,19 +2,34 @@ var textfield
 
 function setup() {
   // put setup code here
-  noCanvas();
-  textfield = createInput();
+  createCanvas(400,400);
+  angleMode(DEGREES);
 
-  textfield.input(newTyping);
-
-}
-function newTyping(){
-  createP(textfield.value());
-}
-function newText(){
-  createP(textfield.value());
 }
 function draw() {
+  background(0);
+
+  let hr = hour();
+  let mn = minute();
+  let sc = second();
+
+   fill(255);
+   noStroke();
+   // if(mn<10){'0'}{
+   //   text(hr + ':0' + mn + ':' + sec , 200, 200);
+   // }else {
+   //   text(hr + ':' + mn + ':' + sec , 200, 200);
+   // }
   // put drawing code here
+
+  strokeWeight(8);
+  stroke(255,100,150);
+  noFill();
+  let end1 = map(sc, 0, 60, 0, 360);
+  arc(200,200,300,300,0,end1);
+
+  let end2 = map(mn, 0, 60, 0, 360);
+  arc(200,200,280,280,0,end2);
+
 
 }
